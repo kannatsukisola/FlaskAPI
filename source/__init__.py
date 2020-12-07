cc632de7f531abf3d655c46e0a1fbb6eb4563ed9
RayRi <673479663@qq.com>
2020-12-07 11:02:13 +0800

    Add Flask API Frame

--- source/__init__.py
+++ source/__init__.py
@@ -1,1 +1,12 @@
 #coding:utf8
+import flask
+import logging
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+logger = logging.getLogger(name="main")
+
+#TODO:链接测试
+
+
+
---
+++ source/lib/base.py
@@ -0,0 +1,4 @@
+#coding:utf8
+
+
+# TODO: 检验 post 方法中 request 的参数有效
