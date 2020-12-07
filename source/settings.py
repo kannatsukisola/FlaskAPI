466c6c7b7a09f0de209895c8740d38621a344b2b
RayRi <673479663@qq.com>
2020-12-07 16:26:27 +0800

    feat($configuration): Add Configuration
    添加基本配置信息

---
+++ source/settings.py
@@ -0,0 +1,17 @@
+#coding:utf8
+"""
+Flask API 需要的所有配置信息
+"""
+# FLASK APP host and port
+HOST = "0.0.0.0"
+PORT = 5000
+
+# Setup Flask APP DEBUG Status
+APP_DEBUG = True
+
+# Setup Flask APP Name
+APP_NAME = "main"
+
+# Log Config
+LOG_ENABLED = True
+LOG_PATH = "log"
