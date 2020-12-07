12452a32b3122ae332d23edd495c92df680bfeb3
RayRi <673479663@qq.com>
2020-12-07 11:44:40 +0800

    feat($home): Add Home Page
    添加主页 endpoint 用于测试服务器连接

--- source/__init__.py
+++ source/__init__.py
@@ -6,7 +6,17 @@ from flask import request, jsonify
 app = flask.Flask(__name__)
 logger = logging.getLogger(name="main")
 
-#TODO:链接测试
+#TODO:连接测试
+@app.route("/", methods=["GET", "POST"])
+def home():
+    """测试服务器是否可以连接
+    """
+    result = jsonify({
+        "description": "连接 API 服务器成功"
+    })
+    logger.debug("连接到测试服务器")
+    return result
 
 
-
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
