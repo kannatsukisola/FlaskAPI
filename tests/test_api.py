eceed59a788a5b71f0979cc2dd14867ba9c936ba
RayRi <673479663@qq.com>
2020-12-07 16:32:32 +0800

    test($request): Request With Parameters
    测试带参数请求

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,22 +2,22 @@
 """
 测试 Flask API
 """
+from flask.globals import request
 import pytest
 import requests
 
 import sys
 sys.path.append("..")
 
+from source.settings import HOST, PORT
 
 
-host="0.0.0.0"
-port=5000
 def test_connect():
     """测试flask 是否可以连接
 
     直接测试主页
     """
-    url = f"http://{host}:{port}/"
+    url = f"http://{HOST}:{PORT}/"
     res = requests.get(url)
     
     assert res.status_code == 200, "连接不成功"
@@ -30,7 +30,18 @@ class TestAPI:
         """测试配置解析
         """
         from source.utils import SETTINGS
-        from source.settings import HOST, PORT
 
         assert SETTINGS["HOST"] == HOST, "解析方法不正确"
         assert SETTINGS["PORT"] == PORT, "解析方法不正确"
+
+
+    def test_request_parameters(self):
+        """测试带请求参数
+        """
+        id_ = 1234
+        url = f"http://{HOST}:{PORT}/args?id={id_}"
+        res = requests.get(url)
+        assert res.json()["id"] == str(id_), "带参数 GET 请求不成功"
+
+        res = requests.post(url)
+        assert res.json()["id"] == str(id_), "带参数 POST 请求不成功"
