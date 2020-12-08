3a0724e69eb121af158e8d565cb8cd4d97fb4081
huangjh <huangjh@isvtop.com>
2020-12-08 11:18:41 +0800

    单点导航

---
+++ source/utils/move_fun.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+""" 单点导航
+"""
+
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
+import rospy
+import math
+from geometry_msgs.msg import PointStamped, PoseStamped
+import actionlib
+from move_base_msgs.msg import *
+import time
+
+
+def move_to_point(point_x, point_y):           #移动到一个点
+    pose = PoseStamped()
+    pose.header.frame_id = 'map'
+    pose.header.stamp = rospy.Time.now()
+    pose.pose.position.x = point_x
+    pose.pose.position.y = point_y
+    pose.pose.orientation.w = 1
+    goal_pub.publish(pose)
+
+
+def send_mark(point_x, point_y):
+    global goal_pub
+    rospy.init_node('path_point_demo')
+    goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size = 1)
+    time.sleep(2)
+    move_to_point(point_x, point_y)
+
